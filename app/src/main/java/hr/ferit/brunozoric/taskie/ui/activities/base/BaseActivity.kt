7e9eec59f19cc62785bb74286ebc27bd7c810e29
pyroblaster <abartulovic@etfos.hr>
2019-05-17 22:10:59 +0200

    aktiviti

--- app/src/main/java/hr/ferit/brunozoric/taskie/ui/activities/base/BaseActivity.kt
+++ app/src/main/java/hr/ferit/brunozoric/taskie/ui/activities/base/BaseActivity.kt
@@ -1,16 +1,13 @@
 package hr.ferit.brunozoric.taskie.ui.activities.base
 
 import android.os.Bundle
-import android.view.MenuItem
+
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import hr.ferit.brunozoric.taskie.R
 import hr.ferit.brunozoric.taskie.common.showFragment
-import hr.ferit.brunozoric.taskie.ui.fragments.ViewPagerFragment
-import hr.ferit.brunozoric.taskie.ui.fragments.TasksFragment
 
-abstract class BaseActivity: AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener{
+abstract class BaseActivity: AppCompatActivity(){
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,17 +24,4 @@ abstract class BaseActivity: AppCompatActivity(), BottomNavigationView.OnNavigat
 
     abstract fun getLayoutResourceId(): Int
     abstract fun setUpUi()
-
-    override fun onNavigationItemSelected(item: MenuItem): Boolean {
-        when(item.itemId){
-            R.id.navigation_tasks ->{
-                showFragment(R.id.fragmentContainer, TasksFragment())
-            }
-            R.id.navigation_about ->{
-                showFragment(R.id.fragmentContainer, ViewPagerFragment())
-            }
-
-        }
-        return true
-    }
 }
